09e121ca990a7fb68788384569e5605025dffe8c
Sascha-netizen <sascha.r.klement@gmail.com>
2025-08-06 15:36:36 +0200

    Add JS dice/number generator.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-console.log('index.js is loaded and running!');
+let dice = ["1 / One", "2 / Two", "3 / Three", "4 / Four", "5 / Five", "6 / Six"];
+
+
+
+function rollDice () {
+let index = Math.ceil(Math.random() * 6) -1;
+let chosenNumber = dice[index]; document.getElementById("diceResult").textContent = chosenNumber;
+}
+
+document.getElementById('rollDiceBtn').addEventListener('click', rollDice);
